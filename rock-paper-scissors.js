a3ae1a505f7be74afad1f2991468a173573f6142
David Singleton <davidjsingleton18@gmail.com>
2023-01-22 16:49:38 -0800

    Add player selection and define computer selection clearly

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,9 +2,31 @@ const playerScore = 0;
 const computerScore = 0;
 
 function computerSelection() {
-  const choices = ['rock', 'paper', 'scissors'];
+  const computerChoices = ['rock', 'paper', 'scissors'];
 
-  const choice = choices[Math.floor(Math.random() * choices.length * 1)];
+  const choice =
+    computerChoices[Math.floor(Math.random() * computerChoices.length * 1)];
 
   return choice;
+}
+
+function playerSelection() {
+  let playerChoices;
+  let playerChoice;
+
+  do {
+    playerChoices = prompt('Enter rock, paper or scissors');
+
+    if (playerChoices === null) {
+      return 0;
+    }
+
+    playerChoice = playerChoices.toLowerCase();
+  } while (
+    playerChoice !== 'rock' &&
+    playerChoice !== 'paper' &&
+    playerChoice !== 'scissors'
+  );
+
+  return playerChoice;
 }
